236357d2448002797856bc7ce6d2aa691987f976
Crista Lopes <lopes@ics.uci.edu>
2019-08-13 09:37:44 -0700

    ahem

--- words.py
+++ words.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
 # An implementation of deep learning for counting symbols
-E.g. with N = 2
 Input:  [10, 12, 10, 11, 2, 2, 2, 1, 1]
 Output: words=[2, 10, 1, 12, 11] counts=[3, 2, 2, 1, 1] (Not necessarily in this order)
 
